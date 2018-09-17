b345eb98c77e8c3757d85e5b2ee8776aa3ad9001
Molkobain <lajarige.guillaume@free.fr>
2018-09-17 21:37:19 +0200

    PHP Documentation

--- dist/molkobain-fontawesome5-pack/console/consoleuiextension.class.inc.php
+++ dist/molkobain-fontawesome5-pack/console/consoleuiextension.class.inc.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Copyright (c) 2015 - 2018 Molkobain.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 namespace Molkobain\iTop\Console\FontAwesome\Extension;
 
@@ -6,12 +15,15 @@ use utils;
 use iTopWebPage;
 use iPageUIExtension;
 
+/**
+ * Class ConsoleUIExtension
+ *
+ * @package Molkobain\iTop\Console\FontAwesome\Extension
+ */
 class ConsoleUIExtension implements iPageUIExtension
 {
     /**
-     * Add content to the North pane
-     * @param iTopWebPage $oPage The page to insert stuff into.
-     * @return string The HTML content to add into the page
+     * @inheritdoc
      */
     public function GetNorthPaneHtml(iTopWebPage $oPage)
     {
@@ -20,9 +32,7 @@ class ConsoleUIExtension implements iPageUIExtension
     }
 
     /**
-     * Add content to the South pane
-     * @param iTopWebPage $oPage The page to insert stuff into.
-     * @return string The HTML content to add into the page
+     * @inheritdoc
      */
     public function GetSouthPaneHtml(iTopWebPage $oPage)
     {
@@ -30,9 +40,7 @@ class ConsoleUIExtension implements iPageUIExtension
     }
 
     /**
-     * Add content to the "admin banner"
-     * @param iTopWebPage $oPage The page to insert stuff into.
-     * @return string The HTML content to add into the page
+     * @inheritdoc
      */
     public function GetBannerHtml(iTopWebPage $oPage)
     {
